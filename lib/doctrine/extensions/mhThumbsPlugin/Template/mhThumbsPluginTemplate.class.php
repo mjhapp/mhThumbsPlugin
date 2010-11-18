abb08f60f590b81c59904ed4bd0be3eb23d27f03
Marshall Happ <marshallhapp@gmail.com>
2010-11-18 15:58:29 -0500

    Added css and images plus Template

---
+++ lib/doctrine/extensions/mhThumbsPlugin/Template/mhThumbsPluginTemplate.class.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * This file is part of the mhThumbsPlugin package.
+ */
+
+/**
+ * Cache class that stores cached content in APC.
+ *
+ * @package    mhThumbsPlugin
+ * @subpackage
+ * @author     Marshall Happ <marshallhapp@gmail.com>
+ * @version    0.1
+ */
+
+// mhThumbsPlugin/lib/doctrine/extensions/mhThumbsPlugin/Template/mhThumbsPluginTemplate.class.php
+
+class ThumbsUpDown extends Doctrine_Template
+{
+  public function setTableDefinition()
+  {
+    $this->hasColumn('vote_ups', 'integer', 0);
+    $this->hasColumn('vote_downs', 'integer', 0);
+  }
+
+  public function setUp()
+  {
+  }
+
+  public function renderVotes()
+  {
+    $votesHTML = '<span class="votes"><span class="voteupimage">'
+      . '<img src="/theme/images/up.png">'
+      . '</span><span class="voteups">'
+      . $this->getInvoker()->getVoteUps()
+      . '</span><span class="votedownimage">'
+      . '<img src="/theme/images/down.png">'
+      . '</span><span class="votedowns">'
+      . $this->getInvoker()->getVoteDowns()
+      . '</span></span>';
+
+    return $votesHTML;
+  }
+}
+
+
+?>
